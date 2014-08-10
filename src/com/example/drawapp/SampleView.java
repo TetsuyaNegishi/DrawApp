ecea3e12668766ba156b94e788574926c87912c9
TetsuyaNegishi <yagamiraito1009@gmail.com>
2014-08-10 14:57:34 +0900

    TouchEvent

--- src/com/example/drawapp/SampleView.java
+++ src/com/example/drawapp/SampleView.java
@@ -4,19 +4,26 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 public class SampleView extends View {
 	
 	private Paint paint = new Paint();
 	public SampleView(Context context) {
 		super(context);
-		setBackgroundColor(Color.BLUE);
+		setBackgroundColor(Color.WHITE);
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas){
-		canvas.drawText("Hello", 100, 100, paint);
+		canvas.drawCircle(100, 100, 20, paint);
 	}
 	
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
+		Toast.makeText(getContext(), "Touch", Toast.LENGTH_SHORT).show();
+		return true;
+	}
 }
