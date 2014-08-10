b03b75f9e862ed788a982ce86ed709258c62a80c
TetsuyaNegishi <yagamiraito1009@gmail.com>
2014-08-10 16:13:49 +0900

    Touch Change Circle Color

--- src/com/example/drawapp/SampleView.java
+++ src/com/example/drawapp/SampleView.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 public class SampleView extends View {
 	
 	private Paint paint = new Paint();
+	private int color = Color.RED;
+	private int bx = 100;
+	private int by = 100;
+	private int R = 50;
 	public SampleView(Context context) {
 		super(context);
 		setBackgroundColor(Color.WHITE);
@@ -18,12 +22,26 @@ public class SampleView extends View {
 	
 	@Override
 	public void onDraw(Canvas canvas){
-		canvas.drawCircle(100, 100, 20, paint);
+		paint.setColor(color);
+		canvas.drawCircle(100, 100, R, paint);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
-		Toast.makeText(getContext(), "Touch", Toast.LENGTH_SHORT).show();
+		int action = event.getAction();
+		if((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN){
+			int ex = (int)event.getX();
+			int ey = (int)event.getY();
+			if((bx - ex)*(bx - ex)+(by - ey)*(by - ey) <= R*R){
+				Toast.makeText(getContext(),"Touch‚³‚ê‚Ü‚µ‚½", Toast.LENGTH_SHORT).show();
+				if(color == Color.BLUE){
+					color = Color.RED;
+				} else {
+					color = Color.BLUE;
+				}
+				invalidate();	// Ä•`‰æ
+			}
+		}
 		return true;
 	}
 }
