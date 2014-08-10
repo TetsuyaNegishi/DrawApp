8d617814889f262f9b62dc2318717ee2aca5d4a2
TetsuyaNegishi <yagamiraito1009@gmail.com>
2014-08-10 17:30:15 +0900

    Move Circle

--- src/com/example/drawapp/MainActivity.java
+++ src/com/example/drawapp/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.drawapp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 //import android.view.Menu;
 //import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-
+	private Handler handler = new Handler();
+	View view;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		View view = new SampleView(this);
+		
+		view = new SampleView(this);
 		setContentView(view);
+		
+		Timer timer = new Timer(false);
+	
+		timer.schedule(new TimerTask(){//–³–¼ƒNƒ‰ƒX
+			public void run(){
+				handler.post(new Runnable(){
+					public void run(){
+						view.invalidate();
+					}
+				});
+			}
+		}, 0, 30);
 	}
 }
--- src/com/example/drawapp/SampleView.java
+++ src/com/example/drawapp/SampleView.java
@@ -1,6 +1,9 @@
 package com.example.drawapp;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,17 +17,40 @@ public class SampleView extends View {
 	private int color = Color.RED;
 	private int bx = 100;
 	private int by = 100;
-	private int R = 50;
+	private int Radius = 20;
+	private int dx = 2;
+	private int dy = 2;
+	private static int margin = 20;
+	private Bitmap item;
+	
 	public SampleView(Context context) {
 		super(context);
 		setBackgroundColor(Color.WHITE);
+		Resources res = context.getResources();
+		item = BitmapFactory.decodeResource(res,  R.drawable.ic_launcher);
 	}
 	
 	@Override
 	public void onDraw(Canvas canvas){
 		paint.setColor(color);
-		canvas.drawCircle(100, 100, R, paint);
+		canvas.drawCircle(bx, by, Radius, paint);
+		
+		//左端に来たら反転
+		if(bx < 0 + margin)
+			dx = 2;
+		//右端
+		if(bx > getWidth() - margin)
+			dx = -2;
+		//上端
+		if(by < 0 + margin)
+			dy = 2;
+		//下端
+		if(by > getHeight() - margin)
+			dy = -2;
+		bx = bx + dx;
+		by = by + dy;
 	}
+
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
@@ -32,7 +58,7 @@ public class SampleView extends View {
 		if((action & MotionEvent.ACTION_MASK) == MotionEvent.ACTION_DOWN){
 			int ex = (int)event.getX();
 			int ey = (int)event.getY();
-			if((bx - ex)*(bx - ex)+(by - ey)*(by - ey) <= R*R){
+			if((bx - ex)*(bx - ex)+(by - ey)*(by - ey) <= Radius*Radius){
 				Toast.makeText(getContext(),"Touchされました", Toast.LENGTH_SHORT).show();
 				if(color == Color.BLUE){
 					color = Color.RED;
